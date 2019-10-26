e904bf77ea88e33ff63f5226958733a53d7b0c6c
Kurian, Abhijith <51823419+PenielAmberstone@users.noreply.github.com>
2019-10-26 21:41:17 +0530

    Initial page setup for hosttest
    added elements to root

---
+++ ContentSupport/ContentLayout/UserProfile/JavaScript/test.js
@@ -0,0 +1,5 @@
+$(document).ready(function()
+{
+    $('#asideProfileNav').load("/ContentBase/SiteCommon/UserAsideProfileNav.html");
+    $('#divBrandName').load("/ContentBase/SiteCommon/UserBrandName.html");
+});
